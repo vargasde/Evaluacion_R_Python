2e259861f25e17da3a441551d7dfb5c4acb917de
Diego Vargas <47101879+vargasde@users.noreply.github.com>
2019-02-25 00:15:41 +0100

    Upload

--- CodigoR.R
+++ CodigoR.R
@@ -91,59 +91,59 @@ for (i in 1:nrow(reasons)) {
                         *ABANDONA EL PUESTO*|*ABANDONA EL VHO*|*ABANDONA VHO*|*ABANDONAR EL VHO*|*ABANDONAR VHO*|
                         *ABANDONAR EL PUESTO*|*ABANDONAR PUESTO*|*ABANDONA COCHE*", reasons$Reason[c]) == TRUE)) {
     reasons$Motivo[c] <- 'ESTACIONAMIENTO NEGLIGENTE'
-} else if(print(grepl("*SENTIDO CONTRARIO*|*CONTRARIA*|*DIRECCION PROHIBIDA*|*SENTIDO PROHIBIDO*|*DIRECCIÓN PROHIBIDO*", reasons$Reason[c]) == TRUE)) {
-  reasons$Motivo[c] <- 'CIRCULACION EN SENTIDO CONTRARIO O PROHIBIDO'
-} else if(print(grepl("*VELOCIDAD*|*ACELER*|\\bMAS DE\\b|\\bCIRCULA A\\b", reasons$Reason[c]) == TRUE)) {
-  reasons$Motivo[c] <- 'VELOCIDAD O ACELERACION INDEBIDA'
-} else if(print(grepl("*PRIORIDAD DE PASO*|*PREFERENCIA DE PASO*", reasons$Reason[c]) == TRUE)) {
-  reasons$Motivo[c] <- 'PRIORIDAD DE PASO' 
-} else if(print(grepl("*SEMAFORO ROJO*|*SEMAFORO EN ROJO*|*FASE ROJA*|*REBASAR*SEMAFOROS*|*SALTA*SEMAFORO*|*SEMAFORO*", reasons$Reason[c]) == TRUE)) {
-  reasons$Motivo[c] <- 'ILEGALIDAD EN SEMAFOROS'
-} else if(print(grepl("*REBASAR*|*ADELANTAR*", reasons$Reason[c]) == TRUE)) {
-  reasons$Motivo[c] <- 'REBASAR INDEBIDAMENTE'
-} else if(print(grepl("*SEÑAL*|*VIALES*|*CIRCULACIÓN RESERVADA*", reasons$Reason[c]) == TRUE)) {
-  reasons$Motivo[c] <- 'OMISION DE SENALIZACION VIAL O DE CONDUCCION'
-} else if(print(grepl("*OCUPANTE*", reasons$Reason[c]) == TRUE)) {
-  reasons$Motivo[c] <- 'EXCESO DE OCUPANTES'
-} else if(print(grepl("*PANTALLA*|*TELEFONO*", reasons$Reason[c]) == TRUE)) {
-  reasons$Motivo[c] <- 'USO DE DISPOSITIVO ELECTRONICO'
-} else if(print(grepl("\\bMOVIL\\b|*OTROS DISPOSITIVOS*", reasons$Reason[c]) == TRUE)) {
-  reasons$Motivo[c] <- 'USO DE DISPOSITIVO ELECTRONICO'
-} else if(print(grepl("*NIÑ@*|*MENOR*", reasons$Reason[c]) == TRUE)) {
-  reasons$Motivo[c] <- 'PELIGRO A MENORES'
-} else if(print(grepl("\\bKM\\b|\\bKMH\\b|\\bKMS\\b", reasons$Reason[c]) == TRUE)) {
-  reasons$Motivo[c] <- 'VELOCIDAD O ACELERACION INDEBIDA'
-} else if(print(grepl("*CARGA*|*DESCARGA*|*ANIMALES*", reasons$Reason[c]) == TRUE)) {
-  reasons$Motivo[c] <- 'TRANSPORTE DE PASAJEROS, OBJETOS O ANIMALES'
-} else if(print(grepl("*CINTUR*", reasons$Reason[c]) == TRUE)) {
-  reasons$Motivo[c] <- 'CINTURON DE SEGURIDAD'
-} else if(print(grepl("*DROGA*", reasons$Reason[c]) == TRUE)) {
-  reasons$Motivo[c] <- 'DROGAS'
-} else if(print(grepl("\\bCASCO\\b", reasons$Reason[c]) == TRUE)) {
-  reasons$Motivo[c] <- 'EQUIPO PROTECTOR'
-} else if(print(grepl("*MARCHA ATR*", reasons$Reason[c]) == TRUE)) {
-  reasons$Motivo[c] <- 'MARCHA ATRAS'
-} else if(print(grepl("*PEATONES*", reasons$Reason[c]) == TRUE)) {
-  reasons$Motivo[c] <- 'OBSTACULIZACION A PEATONES'
-} else if(print(grepl("*ZIG ZAG*", reasons$Reason[c]) == TRUE)) {
-  reasons$Motivo[c] <- 'CONDUCCION NEGLIGENTE ZIG ZAG'
-} else if(print(grepl("\\bPARAR\\b|\\bPARARSE\\b|\\bALTO\\b", reasons$Reason[c]) == TRUE)) {
-  reasons$Motivo[c] <- 'OMISION DE SENALIZACION VIAL O DE CONDUCCION' #Indicar que esto puede ser porque para donde no debe o porque no para cuando le indican.
-} else if(print(grepl("*CAMI?N*", reasons$Reason[c]) == TRUE)) {
-  reasons$Motivo[c] <- 'EXCESO DE PESO CAMION'
-} else if(print(grepl("\\bPERSONAS\\b", reasons$Reason[c]) == TRUE)) {
-  reasons$Motivo[c] <- 'TRANSPORTE DE PASAJEROS, OBJETOS O ANIMALES'
-} else if(print(grepl("*CARRIL*", reasons$Reason[c]) == TRUE)) {
-  reasons$Motivo[c] <- 'USO INDEBIDO DE CARRILES'
-} else if(print(grepl("*BICICLETA*|*MONOPATINES*", reasons$Reason[c]) == TRUE)) {
-  reasons$Motivo[c] <- 'OTROS BICICLETAS'
-} else if(print(grepl("*MONOPATINES*", reasons$Reason[c]) == TRUE)) {
-  reasons$Motivo[c] <- 'OTROS MONOPATINES'
-} else if(print(grepl("*MOTOCICLETA*|*CICLOMOTOR", reasons$Reason[c]) == TRUE)) {
-  reasons$Motivo[c] <- 'OTROS MOTOCICLETA Y CICLOMOTOR'
-} else {
-  reasons$Motivo[c] <- 'OTROS'
-}
+  } else if(print(grepl("*SENTIDO CONTRARIO*|*CONTRARIA*|*DIRECCION PROHIBIDA*|*SENTIDO PROHIBIDO*|*DIRECCIÓN PROHIBIDO*", reasons$Reason[c]) == TRUE)) {
+    reasons$Motivo[c] <- 'CIRCULACION EN SENTIDO CONTRARIO O PROHIBIDO'
+  } else if(print(grepl("*VELOCIDAD*|*ACELER*|\\bMAS DE\\b|\\bCIRCULA A\\b", reasons$Reason[c]) == TRUE)) {
+    reasons$Motivo[c] <- 'VELOCIDAD O ACELERACION INDEBIDA'
+  } else if(print(grepl("*PRIORIDAD DE PASO*|*PREFERENCIA DE PASO*", reasons$Reason[c]) == TRUE)) {
+    reasons$Motivo[c] <- 'PRIORIDAD DE PASO' 
+  } else if(print(grepl("*SEMAFORO ROJO*|*SEMAFORO EN ROJO*|*FASE ROJA*|*REBASAR*SEMAFOROS*|*SALTA*SEMAFORO*|*SEMAFORO*", reasons$Reason[c]) == TRUE)) {
+    reasons$Motivo[c] <- 'ILEGALIDAD EN SEMAFOROS'
+  } else if(print(grepl("*REBASAR*|*ADELANTAR*", reasons$Reason[c]) == TRUE)) {
+    reasons$Motivo[c] <- 'REBASAR INDEBIDAMENTE'
+  } else if(print(grepl("*SEÑAL*|*VIALES*|*CIRCULACIÓN RESERVADA*", reasons$Reason[c]) == TRUE)) {
+    reasons$Motivo[c] <- 'OMISION DE SENALIZACION VIAL O DE CONDUCCION'
+  } else if(print(grepl("*OCUPANTE*", reasons$Reason[c]) == TRUE)) {
+    reasons$Motivo[c] <- 'EXCESO DE OCUPANTES'
+  } else if(print(grepl("*PANTALLA*|*TELEFONO*", reasons$Reason[c]) == TRUE)) {
+    reasons$Motivo[c] <- 'USO DE DISPOSITIVO ELECTRONICO'
+  } else if(print(grepl("\\bMOVIL\\b|*OTROS DISPOSITIVOS*", reasons$Reason[c]) == TRUE)) {
+    reasons$Motivo[c] <- 'USO DE DISPOSITIVO ELECTRONICO'
+  } else if(print(grepl("*NIÑ@*|*MENOR*", reasons$Reason[c]) == TRUE)) {
+    reasons$Motivo[c] <- 'PELIGRO A MENORES'
+  } else if(print(grepl("\\bKM\\b|\\bKMH\\b|\\bKMS\\b", reasons$Reason[c]) == TRUE)) {
+    reasons$Motivo[c] <- 'VELOCIDAD O ACELERACION INDEBIDA'
+  } else if(print(grepl("*CARGA*|*DESCARGA*|*ANIMALES*", reasons$Reason[c]) == TRUE)) {
+    reasons$Motivo[c] <- 'TRANSPORTE DE PASAJEROS, OBJETOS O ANIMALES'
+  } else if(print(grepl("*CINTUR*", reasons$Reason[c]) == TRUE)) {
+    reasons$Motivo[c] <- 'CINTURON DE SEGURIDAD'
+  } else if(print(grepl("*DROGA*", reasons$Reason[c]) == TRUE)) {
+    reasons$Motivo[c] <- 'DROGAS'
+  } else if(print(grepl("\\bCASCO\\b", reasons$Reason[c]) == TRUE)) {
+    reasons$Motivo[c] <- 'EQUIPO PROTECTOR'
+  } else if(print(grepl("*MARCHA ATR*", reasons$Reason[c]) == TRUE)) {
+    reasons$Motivo[c] <- 'MARCHA ATRAS'
+  } else if(print(grepl("*PEATONES*", reasons$Reason[c]) == TRUE)) {
+    reasons$Motivo[c] <- 'OBSTACULIZACION A PEATONES'
+  } else if(print(grepl("*ZIG ZAG*", reasons$Reason[c]) == TRUE)) {
+    reasons$Motivo[c] <- 'CONDUCCION NEGLIGENTE ZIG ZAG'
+  } else if(print(grepl("\\bPARAR\\b|\\bPARARSE\\b|\\bALTO\\b", reasons$Reason[c]) == TRUE)) {
+    reasons$Motivo[c] <- 'OMISION DE SENALIZACION VIAL O DE CONDUCCION' #Indicar que esto puede ser porque para donde no debe o porque no para cuando le indican.
+  } else if(print(grepl("*CAMI?N*", reasons$Reason[c]) == TRUE)) {
+    reasons$Motivo[c] <- 'EXCESO DE PESO CAMION'
+  } else if(print(grepl("\\bPERSONAS\\b", reasons$Reason[c]) == TRUE)) {
+    reasons$Motivo[c] <- 'TRANSPORTE DE PASAJEROS, OBJETOS O ANIMALES'
+  } else if(print(grepl("*CARRIL*", reasons$Reason[c]) == TRUE)) {
+    reasons$Motivo[c] <- 'USO INDEBIDO DE CARRILES'
+  } else if(print(grepl("*BICICLETA*|*MONOPATINES*", reasons$Reason[c]) == TRUE)) {
+    reasons$Motivo[c] <- 'OTROS BICICLETAS'
+  } else if(print(grepl("*MONOPATINES*", reasons$Reason[c]) == TRUE)) {
+    reasons$Motivo[c] <- 'OTROS MONOPATINES'
+  } else if(print(grepl("*MOTOCICLETA*|*CICLOMOTOR", reasons$Reason[c]) == TRUE)) {
+    reasons$Motivo[c] <- 'OTROS MOTOCICLETA Y CICLOMOTOR'
+  } else {
+    reasons$Motivo[c] <- 'OTROS'
+  }
   c = c + 1
 }
 
